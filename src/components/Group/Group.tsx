d44ec0fd866081d0e77c745a52939817f8fcf992
puhalenko <puhalenko@a-3.ru>
2019-10-30 13:34:07 +0300

    feat(3527): add types
    https://jira.a-3.ru/browse/PRJ-3527
    Компонент Group

--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 import styles from './Group.module.css';
+type GroupProps = React.BaseHTMLAttributes<HTMLSpanElement> & {
+    dataTestId?: string;
+};
 
-export function Group(props: React.BaseHTMLAttributes<HTMLSpanElement>): JSX.Element {
-    const { children } = props;
-    return <div className={styles.group}>{children}</div>;
+export function Group(props: GroupProps): JSX.Element {
+    const { children, dataTestId = 'Group-Container' } = props;
+    return (
+        <div className={styles.group} data-test-id={dataTestId}>
+            {children}
+        </div>
+    );
 }
