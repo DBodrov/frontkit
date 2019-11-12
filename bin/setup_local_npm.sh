49801d28d622f694dd80bdfa9fbc3afa4c5329ce
Ilya Sharonov <sharonov@a-3.ru>
2019-11-12 13:21:41 +0300

    ci(project): fixed npm login script
    Metadata: PRJ-3596

--- bin/setup_local_npm.sh
+++ bin/setup_local_npm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-npm config set registry $NPM_REGISTRY
-npm config set always-auth true
-
 yarn global add npm-cli-adduser
 npm-cli-adduser
+
+npm config set registry $NPM_REGISTRY
+npm config set always-auth true
