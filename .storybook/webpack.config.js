2cb7cf6cf5013ca697c354075ff15203bcf8c74c
Andrey Vassilyev <vasiliev@a-3.ru>
2019-11-21 10:00:16 +0300

    fix webpack plugin for ie11 support

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,18 @@
+const path = require('path');
+
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.tsx?$/,
-    use: [
-      {
-        loader: require.resolve("babel-loader"),
-        options: {
-          presets: [require.resolve("babel-preset-react-app")]
-        }
-      },
-      require.resolve("react-docgen-typescript-loader")
-    ]
-  });
+    config.module.rules.push({
+        test: /\.tsx?$/,
+        use: [
+            {
+                loader: require.resolve('babel-loader'),
+                options: { babelrc: true, extends: path.resolve(__dirname, '.babelrc') },
+            },
+            require.resolve('react-docgen-typescript-loader'),
+        ],
+    });
 
-  config.resolve.extensions.push(".ts", ".tsx");
+    config.resolve.extensions.push('.ts', '.tsx');
 
-  return config;
+    return config;
 };
