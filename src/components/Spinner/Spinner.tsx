e45b82d4978de4445c2245a9429cc6ff359598eb
Polonskiy Timofey <polonsky@a-3.ru>
2019-11-21 17:28:32 +0300

    feat(spinner): add circle style prop

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -10,6 +10,8 @@ interface SpinnerProps extends React.HTMLAttributes<HTMLElement> {
     circleClassName?: string;
     /** Inline style objects passed to spinner wrapper */
     style?: React.StyleHTMLAttributes<HTMLElement>;
+    /** Inline style objects passed to spinner circles */
+    circleStyle?: React.StyleHTMLAttributes<HTMLElement>;
     /** String passed to spinner to change color */
     color?: string;
     /** ID attribute for QA Auto-tests
@@ -21,20 +23,30 @@ interface SpinnerProps extends React.HTMLAttributes<HTMLElement> {
 interface CircleProps extends React.HTMLAttributes<HTMLElement> {
     color: string;
     className?: string;
+    circleStyle?: object;
 }
 
 const creacteCircleClass = (className?: string) => classnames(styles.circle, styles.lineAnimation, className);
 
-const Circle = ({ color, className }: CircleProps) => <div className={creacteCircleClass(className)} style={{ backgroundColor: color }} />;
+const Circle = ({ color, className, circleStyle }: CircleProps) => (
+    <div className={creacteCircleClass(className)} style={{ ...circleStyle, backgroundColor: color }} />
+);
 
-export function Spinner({ className, circleClassName, style, color = '#79b5ff', dataTestId = 'Spinner' }: SpinnerProps): JSX.Element {
+export function Spinner({
+    className,
+    circleClassName,
+    style,
+    circleStyle,
+    color = '#79b5ff',
+    dataTestId = 'Spinner',
+}: SpinnerProps): JSX.Element {
     return (
         <div data-testid={dataTestId} className={className} style={style}>
             <div className={styles.spinner}>
-                <Circle color={color} className={circleClassName} />
-                <Circle color={color} className={circleClassName} />
-                <Circle color={color} className={circleClassName} />
-                <Circle color={color} className={circleClassName} />
+                <Circle color={color} className={circleClassName} circleStyle={circleStyle} />
+                <Circle color={color} className={circleClassName} circleStyle={circleStyle} />
+                <Circle color={color} className={circleClassName} circleStyle={circleStyle} />
+                <Circle color={color} className={circleClassName} circleStyle={circleStyle} />
             </div>
         </div>
     );
