e5f39e06053c3ee6c4d754194d83b703b0abb8d2
Andrey Vassilyev <vasiliev@a-3.ru>
2019-11-21 09:24:52 +0300

    fix(input): create common container for icons

--- src/components/Input/Icons.tsx
+++ src/components/Input/Icons.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import styles from './Icons.module.css';
+import classnames from 'classnames';
 
 export type IconProps = {
     dataTestId?: string;
 };
 export function ErrorIcon({ dataTestId = 'input-error-icon' }: IconProps) {
     return (
-        <div className={styles['error-wrapper']} data-testid={dataTestId}>
+        <div className={styles.wrapper} data-testid={dataTestId}>
             <div className={styles['error-circle']}></div>
             <div className={styles['error-cross']}></div>
         </div>
@@ -15,7 +16,7 @@ export function ErrorIcon({ dataTestId = 'input-error-icon' }: IconProps) {
 
 export function SuccessIcon({ dataTestId = 'input-success-icon' }: IconProps) {
     return (
-        <div className={styles['success-wrapper']} data-testid={dataTestId}>
+        <div className={classnames(styles.wrapper, styles['success-wrapper'])} data-testid={dataTestId}>
             <div className={styles['success-stem']}></div>
             <div className={styles['success-kick']}></div>
         </div>
