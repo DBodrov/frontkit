02d8d36972d06268def34b8711c1350e77954954
puhalenko <puhalenko@a-3.ru>
2019-11-22 09:58:54 +0300

    feat(3546): add button tests
    https://jira.a-3.ru/browse/PRJ-3546
    Компонент Button

---
+++ src/__tests__/Button/Button.test.tsx
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Button } from '../../components/Button';
+
+describe('<Button />', () => {
+    test('should render an Button element', () => {
+        const buttonClass = 'btn';
+        const { getByTestId } = render(<Button className={buttonClass}/>);
+        const button = getByTestId('Button');
+        expect(button).not.toBeNull();
+        expect(button).toHaveClass(buttonClass);
+        expect(button).toHaveClass('btn');
+    });
+    test('should have custom style', () => {
+        const style = { color: '#000000' };
+        const { getByTestId } = render(<Button style={style}/>);
+        const button = getByTestId('Button');
+        expect(button).toHaveStyle('color: #000000');
+    });
+    test('should have children', () => {
+        const buttonText = 'Button';
+        const { getByTestId } = render(<Button>{buttonText}</Button>);
+        const button = getByTestId('Button');
+        expect(button).toHaveTextContent(buttonText);
+    });
+    test('should have data-testid', () => {
+        const buttonTestId = 'newButton';
+        const { getByTestId } = render(<Button dataTestId={buttonTestId}/>);
+        const button = getByTestId(buttonTestId);
+        expect(button).not.toBeNull();
+    });
+});
