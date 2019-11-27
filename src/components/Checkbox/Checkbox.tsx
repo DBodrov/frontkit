ba4ee35bef976a73d14c056ec2f2af4e1b12fbb7
puhalenko <puhalenko@a-3.ru>
2019-11-27 12:28:24 +0300

    feat(3549): add Checkbox
    https://jira.a-3.ru/browse/PRJ-3549
    Компонент Checkbox

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,51 @@
+import classnames from 'classnames';
+import React from 'react';
+import { ThemeContext, ThemeTypes } from '../ThemeProvider';
+import styles from './Checkbox.module.css';
+
+interface CheckboxProps extends React.HTMLAttributes<HTMLElement> {
+    /** Class names passed in order to change styling */
+    className?: string;
+    /** Inline style objects passed */
+    style?: React.StyleHTMLAttributes<HTMLElement>;
+    /** ID attribute for QA Auto-tests
+     * @default Button
+     * */
+    dataTestId?: string;
+    /** Description of checkbox */
+    label?: string;
+    /** ID attribute to change styling
+     * @default Checkbox
+     * */
+    id?: string;
+}
+
+const getFinalStyles = (theme: ThemeTypes) => {
+    if (!theme.styles || !theme.styles.mainColor) {
+        return undefined;
+    }
+    const { mainColor } = theme.styles;
+    return { background: mainColor };
+};
+
+export function Checkbox(props: CheckboxProps): JSX.Element {
+    const theme = React.useContext(ThemeContext);
+    const { dataTestId = 'Checkbox', className, style, label, id = 'Checkbox', ...rest } = props;
+    const finalStyles = getFinalStyles(theme);
+    const cls = classnames(styles.checkboxWrapper, theme.className, className);
+    return (
+        <div className={cls} style={style}>
+            <input type="checkbox" id={id} className={styles.checkbox} data-testid={dataTestId} {...rest} />
+            <label htmlFor={id} className={styles.label} data-testid={dataTestId + '-label'}>
+                <div className={styles.bigBox} data-testid={dataTestId + '-bigBox'}>
+                    <div className={styles.smallBox} style={finalStyles} data-testid={dataTestId + '-smallBox'} />
+                </div>
+                {label && (
+                    <span className={styles.labelText} data-testid={dataTestId + '-label-text'}>
+                        {label}
+                    </span>
+                )}
+            </label>
+        </div>
+    );
+}
