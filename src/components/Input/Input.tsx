fc231480f5683b800e578137be25107202a426f4
Andrey Vassilyev <vasiliev@a-3.ru>
2019-12-02 17:01:54 +0300

    refactor(input): preparation for left icon

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { DependencyList, EffectCallback } from 'react';
 import styles from './Input.module.css';
-import classnames from 'classnames';
+import cn from 'classnames';
 import { IconProps } from './Icons';
 
 export enum BackgroundProp {
@@ -17,28 +17,88 @@ type Props = {
 } & React.HTMLAttributes<HTMLInputElement>;
 
 function getBackgroundClass(background?: BackgroundProp): string {
-    return classnames({
+    return cn({
         [styles.error]: background === BackgroundProp.Error,
         [styles.success]: background === BackgroundProp.Success,
     });
 }
 
+function useOnClickOutside(ref: React.RefObject<HTMLElement | null>, f: EffectCallback, deps: DependencyList): void {
+    React.useEffect(() => {
+        function clickHandler(e: MouseEvent): void {
+            const { current } = ref;
+            if (!current) {
+                return;
+            }
+
+            if (!current.contains(e.target as Node)) {
+                f();
+            }
+        }
+
+        document.addEventListener('click', clickHandler);
+        return (): void => document.removeEventListener('click', clickHandler);
+    }, deps);
+}
+
+function useFocus(): [boolean, () => void, () => void, React.RefObject<HTMLDivElement>] {
+    const [focused, setFocused] = React.useState(false);
+    const inputRef = React.useRef<HTMLDivElement>(null);
+
+    const onFocus = React.useCallback(() => setFocused(true), [setFocused]);
+    const onBlur = React.useCallback(() => setFocused(false), [setFocused]);
+
+    useOnClickOutside(
+        inputRef,
+        () => {
+            if (!focused) {
+                return;
+            }
+
+            setFocused(false);
+        },
+        [focused, setFocused],
+    );
+
+    return [focused, onFocus, onBlur, inputRef];
+}
+
 export function Input({ value, placeholder, Icon, background, className, style, dataTestId, ...rest }: Props): JSX.Element {
+    const [focused, onFocus, onBlur, fieldRef] = useFocus();
+    const inputRef = React.useRef<HTMLInputElement>(null);
+
+    React.useEffect(() => {
+        const { current } = inputRef;
+        if (!current) {
+            return;
+        }
+        if (focused) {
+            current.focus();
+        } else {
+            current.blur();
+        }
+    }, [focused]);
+
+    const backgroundClass = getBackgroundClass(background);
+    const wrapperClassName = cn(styles.wrapper, { [styles.focused]: focused });
+
     return (
-        <div className={styles.wrapper}>
+        <div className={wrapperClassName} ref={fieldRef}>
+            <div className={cn(styles.input, styles.leftArea, backgroundClass)} onClick={onFocus} />
             <input
                 value={value}
                 placeholder={placeholder}
-                className={classnames(className, styles.input, getBackgroundClass(background))}
+                className={cn(styles.input, styles.inputArea, backgroundClass, className)}
                 style={style}
                 data-testid={dataTestId}
+                ref={inputRef}
+                onFocus={onFocus}
+                onBlur={onBlur}
                 {...rest}
             />
-            {Icon && (
-                <div className={styles.icon}>
-                    <Icon />
-                </div>
-            )}
+            <div className={cn(styles.input, styles.rightArea, backgroundClass)} onClick={onFocus}>
+                {Icon && <Icon />}
+            </div>
         </div>
     );
 }
