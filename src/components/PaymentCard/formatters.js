9922399fdd7f0279a3a4a4889fbaffdc77ecfd5f
Polonskiy Timofey <polonsky@a-3.ru>
2019-12-06 18:13:15 +0300

    feat(payment cards): add formatt and valid functions, remove trash

--- src/components/PaymentCard/formatters.js
+++ src/components/PaymentCard/formatters.js
@@ -1,28 +1,23 @@
-export const formatCreditCard = value => value
-  .replace(/\s/g, '') // remove whitespace
-  .slice(0, 19)
-  .replace(/(\d{4})/g, '$1 ') // insert space every 4
-  .replace(/(^\s+|\s+$)/, ''); // truncate sides
+export const formatCreditCard = value =>
+    value
+        .replace(/\s/g, '') // remove whitespace
+        .slice(0, 19)
+        .replace(/(\d{4})/g, '$1 ') // insert space every 4
+        .replace(/(^\s+|\s+$)/, ''); // truncate sides
 
-export const formatDate = value => value
-  .replace('/', '')
-  .replace(/(\d{2})(\d+)/, '$1/$2');
+export const formatDate = value => value.replace('/', '').replace(/(\d{2})(\d+)/, '$1/$2');
 
 export const formatName = value => value.toUpperCase();
 
-export const formatPhone = value => value
-  .replace(/^8/, '')
-  .replace(/.+?\(/, '+7 (') // replace all before (
-  .replace('+7 (7', '+7 (')
-  .replace(/\+7|7?/, '') // remove first +7 || 7
-  .replace(/[^0-9]/g, '') // only digits
-  .slice(0, 10)
-  .replace(/(\d{1,3})?(\d{1,3})?(\d{1,2})?(\d{1,2})?/, '+7 ($1) $2 $3 $4') // add +7()--
-  .replace(/(\) --)|(--?)$/, '') // truncate
-  .replace(/  +/g, ' ');
-
-export const formatMobile = number =>
-  number
-    .replace(/[^0-9]/g, '')
-    .replace(/^[7,8]/, '')
-    .substring(0, 10);
+const format = (name, value) => {
+    switch (name) {
+        case 'cc-number':
+            return formatCreditCard(value);
+        case 'cc-name':
+            return formatName(value);
+        case 'cc-exp':
+            return formatDate(value);
+        default:
+            return value;
+    }
+};
--- src/components/PaymentCard/validators.js
+++ src/components/PaymentCard/validators.js
@@ -1,54 +1,70 @@
 export const REGEXP_NON_DDS = /[^0-9-\s]+/;
 export const REGEXP_NON_DATE = /[^0-9/]+/;
-export const REGEXP_NON_PHONE = /[^0-9+\-\s()]/;
 export const REGEXP_NON_LATIN = /[^a-zA-Z' ]+/;
 export const REGEXP_NON_DIGITS = /[^0-9]+/;
-export const REGEXP_NON_EMAIL = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/;
 
-export const validateCreditCard = (value) => {
-  value = value.replace(/\D/g, '');
+export const validateCreditCard = value => {
+    value = value.replace(/\D/g, '');
 
-  if (value.length < 12) {
-    return false;
-  }
-
-  // The Luhn Algorithm
-  let nCheck = 0;
-  let nDigit = 0;
-  let bEven = false;
-
-  for (let n = value.length - 1; n >= 0; n--) {
-    const cDigit = value.charAt(n);
-    nDigit = parseInt(cDigit, 10);
-
-    if (bEven) {
-      nDigit *= 2;
-      if (nDigit > 9) {
-        nDigit -= 9;
-      }
+    if (value.length < 12) {
+        return false;
     }
 
-    nCheck += nDigit;
-    bEven = !bEven;
-  }
+    // The Luhn Algorithm
+    let nCheck = 0;
+    let nDigit = 0;
+    let bEven = false;
 
-  return nCheck !== 0 && nCheck % 10 === 0;
+    for (let n = value.length - 1; n >= 0; n--) {
+        const cDigit = value.charAt(n);
+        nDigit = parseInt(cDigit, 10);
+
+        if (bEven) {
+            nDigit *= 2;
+            if (nDigit > 9) {
+                nDigit -= 9;
+            }
+        }
+
+        nCheck += nDigit;
+        bEven = !bEven;
+    }
+
+    return nCheck !== 0 && nCheck % 10 === 0;
 };
 
-export const validatePhone = value =>
-  value
-    .slice(2)
-    .replace(/[^0-9]/g, '')
-    .length === 10;
-
-export const validateExp = value => value
-  .replace(/[^0-9]/g, '')
-  .length === 4;
+export const validateExp = value => value.replace(/[^0-9]/g, '').length === 4;
 
 export const validateCvv = value => value.length > 0;
 
-export const validateName = value => value
-  .replace(/\s/g, '')
-  .length !== 0;
+export const validateName = value => value.replace(/\s/g, '').length !== 0;
 
-export const validateEmail = value => !(value === '' || value.indexOf('@') === -1);
+const isInvalidInput = (name, value) => {
+    switch (name) {
+        case 'cc-number':
+            return REGEXP_NON_DDS.test(value);
+        case 'cc-name':
+            return REGEXP_NON_LATIN.test(value);
+        case 'cc-exp':
+            return REGEXP_NON_DATE.test(value);
+        case 'cc-csc':
+            return REGEXP_NON_DIGITS.test(value);
+        default:
+            return false;
+    }
+};
+
+const validate = (name, value) => {
+    switch (name) {
+        case 'cc-number':
+            return validateCreditCard(value);
+        case 'cc-name':
+            return validateName(value);
+        case 'cc-exp':
+            return validateExp(value);
+        case 'cc-csc':
+            return validateCvv(value);
+        default:
+            return true;
+    }
+};
