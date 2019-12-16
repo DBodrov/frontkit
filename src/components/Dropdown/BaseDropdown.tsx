b213f5bddc43be58b0e2361cb3ab375c3887b6c0
Andrey Vassilyev <vasiliev@a-3.ru>
2019-12-16 09:37:30 +0300

    feat(dropdown): styles for not found

--- src/components/Dropdown/BaseDropdown.tsx
+++ src/components/Dropdown/BaseDropdown.tsx
@@ -14,15 +14,21 @@ type ListProps<T> = {
 };
 function List<T>({ data, showMore }: ListProps<T>): JSX.Element {
     return (
-        <>
-            <Card
-                className={styles.card}
-                getSplitType={(splitOrder, size) => (size - 1 === splitOrder && showMore ? SplitType.Full : SplitType.Padding)}
-            >
-                {data}
-                {showMore && <More />}
-            </Card>
-        </>
+        <Card
+            className={styles.card}
+            getSplitType={(splitOrder, size) => (size - 1 === splitOrder && showMore ? SplitType.Full : SplitType.Padding)}
+        >
+            {data}
+            {showMore && <More />}
+        </Card>
+    );
+}
+
+function NotFound(): JSX.Element {
+    return (
+        <Card className={styles.card}>
+            <p>По запросу ничего не найдено</p>
+        </Card>
     );
 }
 
@@ -52,7 +58,7 @@ export function BaseDropdown({ inputValue, data, onChangeInput, type, ...rest }:
                 showOutline={type === Type.InputOnly}
             />
             {type === Type.Data || (type === Type.DataAndMore && <List data={data} showMore={type === Type.DataAndMore} />)}
-            {type === Type.NotFound && <div>not found</div>}
+            {type === Type.NotFound && <NotFound />}
             {type !== Type.InputOnly && <Dimmer />}
         </div>
     );
