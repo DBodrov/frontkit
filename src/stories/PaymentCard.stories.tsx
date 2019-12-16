fa40951acbb893d3233f549eb70cddf589529d10
Timofey <tim.wow.wow1@gmail.com>
2019-12-16 14:52:29 +0300

    feat(payment cards): replace consolelog to storybook action

--- src/stories/PaymentCard.stories.tsx
+++ src/stories/PaymentCard.stories.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import { PaymentCards } from '../components/PaymentCard';
 
-storiesOf('PaymentCard', module).add('PaymentCards', () => <PaymentCards onSuccess={success => console.log(success)} />, {
+storiesOf('PaymentCard', module).add('PaymentCards', () => <PaymentCards onSuccess={action('onSuccess')} />, {
     info: { inline: true },
 });
