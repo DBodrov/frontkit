6fd8ab4a2608f9378e6e4ce66a6a139fd7cc9af6
Timofey <tim.wow.wow1@gmail.com>
2020-01-17 13:24:16 +0300

    feat(masked input): remove comment

--- src/stories/maskedInput.stories.tsx
+++ src/stories/maskedInput.stories.tsx
@@ -3,14 +3,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { MaskedInput } from '../components/MaskedInput';
 
-storiesOf('Masked Input', module).add(
-    'Input',
-    () => (
-        <MaskedInput
-            placeholder="Введите телефон"
-            mask="+7 (999) 999 99 99"
-            // value="+7 (960) 862 22 26"
-        />
-    ),
-    { info: { inline: true } },
-);
+storiesOf('Masked Input', module).add('Input', () => <MaskedInput placeholder="Введите телефон" mask="+7 (999) 999 99 99" />, {
+    info: { inline: true },
+});
