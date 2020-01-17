b7e46843f50a4794604a363460ce398cc85a31a5
Timofey <tim.wow.wow1@gmail.com>
2020-01-17 13:26:35 +0300

    feat(masked input): remove excess prop

--- src/components/MaskedInput/MaskedInput.tsx
+++ src/components/MaskedInput/MaskedInput.tsx
@@ -11,7 +11,7 @@ interface MaskedInputProps extends InputProps {
 export function MaskedInput(props: MaskedInputProps): JSX.Element {
     return (
         <InputMask mask={props.mask} maskChar={null} onChange={props.onChange}>
-            {(): JSX.Element => <Input placeholder={props.placeholder} {...props} />}
+            {(): JSX.Element => <Input {...props} />}
         </InputMask>
     );
 }
