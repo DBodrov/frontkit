5c3c93047ffdacd879131ec5e371be65666f791a
Andrey Vassilyev <vasiliev@a-3.ru>
2020-01-24 06:57:32 +0300

    fix(provider): fix ie11 default margins for figure

--- src/components/Providers/Provider.tsx
+++ src/components/Providers/Provider.tsx
@@ -10,9 +10,19 @@ interface Props {
     style?: object;
     onClick?: () => unknown;
 }
-export function Provider({ src, name, dataTestId, width, style, onClick, ...rest }: Props) {
+// В IE11 для figure установлены margin-ы по умолчанию, которые ломают вёрстку.
+// Сбросить их можно исключительно inline стилями
+const IE11FigureFix = { marginLeft: 0 };
+export function Provider({ src, name, dataTestId, width, style, onClick, ...rest }: Props): JSX.Element {
     return (
-        <figure {...rest} className={styles.wrapper} data-testid={dataTestId} style={{ width, ...style }} onClick={onClick} role="button">
+        <figure
+            {...rest}
+            className={styles.wrapper}
+            data-testid={dataTestId}
+            style={{ width, ...style, ...IE11FigureFix }}
+            onClick={onClick}
+            role="button"
+        >
             <div className={styles.content}>
                 <div data-testid={dataTestId + '-image'} className={cn(styles['image-wrapper'], styles['center-container'])}>
                     <img className={styles.image} src={src} alt={name} />
