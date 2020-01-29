f6fb3b7a78c1d8b7312a52ada18690ea8a416435
Timofey <tim.wow.wow1@gmail.com>
2020-01-29 10:23:26 +0300

    feat(masked input stories): remove prop

--- src/stories/maskedInput.stories.tsx
+++ src/stories/maskedInput.stories.tsx
@@ -3,10 +3,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { MaskedInput } from '../components/MaskedInput';
 
-storiesOf('Masked Input', module).add(
-    'Input',
-    () => <MaskedInput placeholder="Введите телефон" autoFocus={true} mask="+7 (999) 999 99 99" />,
-    {
-        info: { inline: true },
-    },
-);
+storiesOf('Masked Input', module).add('Input', () => <MaskedInput placeholder="Введите телефон" autoFocus mask="+7 (999) 999 99 99" />, {
+    info: { inline: true },
+});
