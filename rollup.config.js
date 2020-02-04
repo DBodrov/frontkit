686177596cdea8102963a9333befbf1577b7f74d
Andrey Vassilyev <vasiliev@a-3.ru>
2020-02-04 14:45:30 +0300

    fix(rollup): fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,18 @@
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import externalPlugin from 'rollup-plugin-auto-external';
 import postcss from 'rollup-plugin-postcss';
+import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
-export default [
-    /* {
-        input: 'src/index.ts',
-        output: {
-            name: 'a3-uikit',
-            file: pkg.browser,
-            format: 'umd',
-        },
-        plugins: [resolve(), commonjs(), typescript(), postcss({ modules: true })],
-    }, */
-    {
-        input: 'src/index.ts',
-        plugins: [resolve(), commonjs(), typescript(), postcss({ modules: true })],
-        output: [/* { file: pkg.main, format: 'cjs' }, */ { file: pkg.module, format: 'es' }],
-    },
-];
+export default {
+    input: 'src/index.ts',
+    plugins: [
+        postcss({ modules: true }),
+        babel({ extensions: ['.ts', '.tsx'] }),
+        commonjs(),
+        resolve({ extensions: ['.ts', '.tsx', '.js'] }),
+        externalPlugin(),
+    ],
+    output: [{ file: pkg.module, format: 'es' }],
+};
