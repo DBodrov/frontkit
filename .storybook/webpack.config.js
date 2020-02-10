6e0b40c5540ebb0b96e477ca6565b8b5a3f467bc
Andrey Vassilyev <vasiliev@a-3.ru>
2020-02-10 15:15:05 +0300

    feat(deps): remove react-scripts

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,37 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
 module.exports = ({ config }) => {
-    config.module.rules.push({
-        test: /\.tsx?$/,
-        use: [
-            {
-                loader: require.resolve('babel-loader'),
-                options: { babelrc: true, extends: path.resolve(__dirname, '.babelrc') },
-            },
-            require.resolve('react-docgen-typescript-loader'),
-        ],
+    config.module.rules.find(rule => rule.test.test('123.css')).exclude = /\.module\.css$/;
+    return merge(config, {
+        module: {
+            rules: [
+                {
+                    test: /\.module\.css$/,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        {
+                            loader: require.resolve('babel-loader'),
+                            options: { babelrc: true, extends: path.resolve(__dirname, '.babelrc') },
+                        },
+                        require.resolve('react-docgen-typescript-loader'),
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx'],
+        },
     });
-
-    config.resolve.extensions.push('.ts', '.tsx');
-
-    return config;
 };
