40254936b3238314cb2dd957efbba0738b18d9c6
Andrey Vassilyev <vasiliev@a-3.ru>
2020-02-13 10:30:35 +0300

    fix(accordion): move utils to utils

--- src/components/Accordion/AccordionDetails.tsx
+++ src/components/Accordion/AccordionDetails.tsx
@@ -5,57 +5,13 @@ import { AccordionDetailsSuccess, Data } from './AccordionDetailsSuccess';
 import styles from './AccordionDetails.module.css';
 import { Spinner } from '../Spinner';
 import { Button } from '../Button';
+import { lazy, RenderPromise } from './utils';
 
 interface DetailsProps {
     id: string;
     getData: () => Promise<ReadonlyArray<Data>>;
     align?: 'left' | 'right';
     dataTestId?: string;
-}
-
-interface RenderPromiseProps<T, P> {
-    promise: Promise<T> | null;
-    successRender: (val: T, props: P) => JSX.Element;
-    errorRender: (err: unknown, props: P, reset: () => unknown) => JSX.Element;
-    loadingRender: (props: P) => JSX.Element;
-    reset: () => unknown;
-    props: P;
-}
-export function RenderPromise<T, P>({
-    promise,
-    reset,
-    successRender,
-    errorRender,
-    loadingRender,
-    props,
-}: RenderPromiseProps<T, P>): JSX.Element {
-    const [loadedData, setLoadedData] = React.useState<React.ComponentType | null>(null);
-
-    React.useEffect(() => {
-        if (promise !== null) {
-            setLoadedData(null);
-        }
-    }, [promise]);
-
-    React.useEffect(() => {
-        if (loadedData === null) {
-            promise
-                ?.then(val => () => successRender(val, props))
-                .catch(err => () => errorRender(err, props, reset))
-                .then(data => {
-                    // Потому что data - это функциональный компонент
-                    // Если выполнить setLoadedData(data), то setLoadedData посчитает data
-                    // функцией, которая изменяет стейт https://reactjs.org/docs/react-component.html#setstate
-                    setLoadedData(() => data);
-                });
-        }
-    }, [promise, setLoadedData, loadedData]);
-
-    if (promise === null) {
-        return <React.Fragment />;
-    }
-
-    return React.createElement(loadedData ?? (() => loadingRender(props)));
 }
 
 type Props = Omit<React.ComponentProps<typeof AccordionDetailsSuccess>, 'data'>;
@@ -80,45 +36,6 @@ function Error(err: unknown, props: Props, reset: () => unknown): JSX.Element {
             </Button>
         </div>
     );
-}
-
-function lazyFactory<T>(factory: () => Promise<T>): () => Promise<T> {
-    let resolve: (arg: T) => void;
-    let reject: (arg: unknown) => void;
-    const cache = new Promise<T>((res, rej) => {
-        resolve = res;
-        reject = rej;
-    });
-    let called = false;
-
-    return () => {
-        if (!called) {
-            factory()
-                .then(resolve)
-                .catch(reject);
-            called = true;
-        }
-        return cache;
-    };
-}
-
-interface LazyResult<T> {
-    lazy: () => Promise<T>;
-    reset: () => unknown;
-}
-export function lazy<T>(factory: () => Promise<T>): LazyResult<T> {
-    let instance = lazyFactory(factory);
-    function reset(): void {
-        instance = lazyFactory(factory);
-    }
-    return {
-        lazy: (): Promise<T> =>
-            instance().catch(e => {
-                reset();
-                return Promise.reject(e);
-            }),
-        reset,
-    };
 }
 
 export function AccordionDetails({ id, getData, align = 'left', dataTestId = 'AccordionDetails' }: DetailsProps): JSX.Element {
