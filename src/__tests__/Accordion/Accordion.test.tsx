d07f631cafbf25d544a340538e713ae6754725cc
Andrey Vassilyev <vasiliev@a-3.ru>
2020-02-13 10:42:57 +0300

    fix(accordion): tests

--- src/__tests__/Accordion/Accordion.test.tsx
+++ src/__tests__/Accordion/Accordion.test.tsx
@@ -2,44 +2,54 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import { Accordion, AccordionLineType } from '../../components/Accordion';
 
-const data: React.ComponentProps<typeof Accordion.Details>['data'] = [
-    {
-        type: AccordionLineType.Header,
-        value: 'header',
-    },
-    {
-        type: AccordionLineType.Bold,
-        name: 'boldTitle',
-        value: 'boldValue',
-    },
-    { type: AccordionLineType.BigMargin },
-    {
-        type: AccordionLineType.Regular,
-        name: 'title',
-        value: 'value',
-    },
-];
+const getData: React.ComponentProps<typeof Accordion.Details>['getData'] = () =>
+    Promise.resolve([
+        {
+            type: AccordionLineType.Header,
+            value: 'header',
+        },
+        {
+            type: AccordionLineType.Bold,
+            name: 'boldTitle',
+            value: 'boldValue',
+        },
+        { type: AccordionLineType.BigMargin },
+        {
+            type: AccordionLineType.Regular,
+            name: 'title',
+            value: 'value',
+        },
+    ]);
 
-const dataWithTestId: React.ComponentProps<typeof Accordion.Details>['data'] = [
-    {
-        type: AccordionLineType.Header,
-        value: 'header',
-        dataTestId: 'header',
-    },
-    {
-        type: AccordionLineType.Bold,
-        name: 'boldTitle',
-        value: 'boldValue',
-        dataTestId: 'bold',
-    },
-    { type: AccordionLineType.BigMargin },
-    {
-        type: AccordionLineType.Regular,
-        name: 'title',
-        value: 'value',
-        dataTestId: 'regular',
-    },
-];
+function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getDelayData = () => sleep(2000).then(getData);
+
+const getDelayError = () => sleep(2000).then(Promise.reject);
+
+const dataWithTestId: React.ComponentProps<typeof Accordion.Details>['getData'] = () =>
+    Promise.resolve([
+        {
+            type: AccordionLineType.Header,
+            value: 'header',
+            dataTestId: 'header',
+        },
+        {
+            type: AccordionLineType.Bold,
+            name: 'boldTitle',
+            value: 'boldValue',
+            dataTestId: 'bold',
+        },
+        { type: AccordionLineType.BigMargin },
+        {
+            type: AccordionLineType.Regular,
+            name: 'title',
+            value: 'value',
+            dataTestId: 'regular',
+        },
+    ]);
 
 describe('<Accordion.Link />', () => {
     const testId = 'Accoooooooooooordion';
@@ -62,45 +72,48 @@ describe('<Accordion.Link />', () => {
 describe('<Accordion.Details />', () => {
     const testId = 'Accoooooooooooordion';
 
-    test('should not rendered while closed', () => {
+    test('should not rendered while closed', async () => {
         const { getByTestId } = render(
             <>
                 <Accordion.Link id="test" />
-                <Accordion.Details id="test" data={data} dataTestId={testId} />
+                <Accordion.Details id="test" getData={getData} dataTestId={testId} />
             </>,
         );
 
         expect(() => getByTestId(testId)).toThrow();
         fireEvent.click(getByTestId('AccordionLink'));
+        await sleep(100);
         expect(() => getByTestId(testId)).not.toThrow();
         fireEvent.click(getByTestId('AccordionLink'));
         expect(() => getByTestId(testId)).toThrow();
     });
 
-    test('should details have default dataTestId', () => {
+    test('should details have default dataTestId', async () => {
         const { getAllByTestId, getByTestId } = render(
             <>
                 <Accordion.Link id="test" />
-                <Accordion.Details id="test" data={data} />
+                <Accordion.Details id="test" getData={getData} />
             </>,
         );
 
         fireEvent.click(getByTestId('AccordionLink'));
+        await sleep(100);
         expect(() => getByTestId('AccordionDetails')).not.toThrow();
         expect(() => getByTestId('AccordionDetails-header')).not.toThrow();
         expect(() => getAllByTestId('AccordionDetails-name')).not.toThrow();
         expect(() => getAllByTestId('AccordionDetails-value')).not.toThrow();
     });
 
-    test('should details render local dataTestId', () => {
+    test('should details render local dataTestId', async () => {
         const { getByTestId } = render(
             <>
                 <Accordion.Link id="test" />
-                <Accordion.Details id="test" data={dataWithTestId} dataTestId={testId} />
+                <Accordion.Details id="test" getData={dataWithTestId} dataTestId={testId} />
             </>,
         );
 
         fireEvent.click(getByTestId('AccordionLink'));
+        await sleep(100);
         expect(() => getByTestId(testId)).not.toThrow();
         expect(() => getByTestId('header' + testId + '-header')).not.toThrow();
         expect(() => getByTestId('bold' + testId + '-name')).not.toThrow();
@@ -109,15 +122,16 @@ describe('<Accordion.Details />', () => {
         expect(() => getByTestId('regular' + testId + '-value')).not.toThrow();
     });
 
-    test('align=right', () => {
+    test('align=right', async () => {
         const { getByTestId } = render(
             <>
                 <Accordion.Link id="test" />
-                <Accordion.Details id="test" align="right" data={dataWithTestId} dataTestId={testId} />
+                <Accordion.Details id="test" align="right" getData={dataWithTestId} dataTestId={testId} />
             </>,
         );
 
         fireEvent.click(getByTestId('AccordionLink'));
+        await sleep(100);
         expect(() => getByTestId(testId)).not.toThrow();
         expect(getByTestId(testId).className).toContain('right');
     });
