93ddf25cea57d83cbac8bcf6a1ec5151e18b8488
Timofey <tim.wow.wow1@gmail.com>
2020-02-14 10:58:25 +0300

    fix(input): color fixes

--- src/components/Input/BaseInput.tsx
+++ src/components/Input/BaseInput.tsx
@@ -19,6 +19,7 @@ interface BaseInputProps extends Props {
 
 function getBackgroundClass(background?: BackgroundProp): string {
     return cn({
+        [styles.defaultInput]: background === undefined,
         [styles.error]: background === BackgroundProp.Error,
         [styles.success]: background === BackgroundProp.Success,
         [styles.white]: background === BackgroundProp.White,
