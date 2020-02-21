aafba68ce5902a50f2d05f9772f16f50bafaa34f
puhalenko <puhalenko@a-3.ru>
2020-02-21 10:32:03 +0300

    feat(modal): close theme color

--- src/stories/modal.stories.tsx
+++ src/stories/modal.stories.tsx
@@ -3,13 +3,20 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Modal } from '../components/Modal';
 import { action } from '@storybook/addon-actions';
+import { ThemeProvider, ThemeTypes } from '../components/ThemeProvider';
+
+const theme: ThemeTypes = {
+    mainColor: '#8000ff',
+};
 
 storiesOf('Modal', module).add(
     'simple Modal',
     () => (
-        <Modal onClose={action('close')} header="1231231">
-            <span>123</span>
-        </Modal>
+        <ThemeProvider value={theme}>
+            <Modal onClose={action('close')} header="1231231">
+                <span>123</span>
+            </Modal>
+        </ThemeProvider>
     ),
     { info: { inline: true } },
 );
