0317cb6e5d556290139dee97fabf854bfc762943
puhalenko <puhalenko@a-3.ru>
2020-03-05 15:33:15 +0300

    feat(4148): spinner color
    Добавить возможность настройки цвета

--- src/components/ThemeProvider/index.tsx
+++ src/components/ThemeProvider/index.tsx
@@ -4,7 +4,7 @@ const defaultTheme = {
     mainColor: '#69A6F1',
     linkColor: '#4B8BDA',
     btnTextColor: '#F2F2F2',
-    loaderColor: '',
+    loaderColor: '#69A6F1',
 };
 
 export type ThemeTypes = typeof defaultTheme;
@@ -17,6 +17,6 @@ interface ThemeProviderProps {
 }
 
 export function ThemeProvider({ value, children }: ThemeProviderProps): JSX.Element {
-    const colors = { ...defaultTheme, ...value };
+    const colors = { ...defaultTheme, ...value, loaderColor: value.loaderColor || value.mainColor };
     return <ThemeContext.Provider value={colors}>{children}</ThemeContext.Provider>;
 }
