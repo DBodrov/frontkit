6b71a00b2569ffc386b0c4fbd99788b01fda9a41
d.bodrov <bodrov@a-3.ru>
2021-10-29 18:47:15 +0300

    ci: debug npmrc

---
+++ scripts/setup.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+function setupNpm() {
+  const NPM_TOKEN = process.env.NPM_TOKEN;
+
+  const content = `
+  @a3:registry=http://192.168.167.51:8081/repository/local-npm/
+  //192.168.167.51:8081/repository/local-npm/:_authToken=${NPM_TOKEN}
+  email=noreply@a-3.ru
+  always-auth=true
+  `;
+  fs.writeFileSync('.npmrc', content);
+}
+
+setupNpm();
